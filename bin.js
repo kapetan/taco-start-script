4056a72eda0f648a29554edbe68e27c7f1fc1689
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2015-10-26 15:10:46 +0100

    Child inherits stdio

--- bin.js
+++ bin.js
@@ -11,10 +11,7 @@ if(argv.help) {
 	var usage = fs.readFileSync(path.join(__dirname, 'usage.txt'), 'utf-8');
 	console.error(usage);
 } else {
-	start(argv.env || process.env.NODE_ENV || 'development', function(err, stdout, stderr) {
+	start(argv.env || process.env.NODE_ENV || 'development', function(err) {
 		if(err) throw err;
-
-		if(stdout) console.log(stdout);
-		if(stderr) console.error(stderr);
 	});
 }
--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 var fs = require('fs');
-var child = require('child_process');
+var proc = require('child_process');
+var eos = require('end-of-stream');
 
-module.exports = function(env, callback) {
+var noop = function() {};
+
+function shell(command, options) {
+	var file = null;
+	var args = null;
+
+	if (process.platform === 'win32') {
+		file = process.env.comspec || 'cmd.exe';
+		args = ['/s', '/c', '"' + command + '"'];
+		options.windowsVerbatimArguments = true;
+	} else {
+		file = '/bin/sh';
+		args = ['-c', command];
+	}
+
+	if(options.shell) file = options.shell;
+	if(!options.stdio) options.stdio = 'inherit';
+
+	return {
+		file: file,
+		args: args,
+		options: options
+	};
+}
+
+module.exports = function(env, options, callback) {
+	if(!callback && typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+
+	options = options || {};
+	callback = callback || noop;
+
 	fs.readFile('package.json', 'utf-8', function(err, pkg) {
 		if(err) return callback(err);
 
@@ -13,7 +47,14 @@ module.exports = function(env, callback) {
 
 		var script = pkg && pkg.scripts && pkg.scripts['start-' + env];
 
-		if(script) child.exec(script, callback);
-		else callback(new Error('Missing start script for ' + env));
+		if(script) {
+			script = shell(script, options);
+			var child = proc.spawn(script.file, script.args, script.options);
+			eos(child, callback);
+
+			return child;
+		} else {
+			callback(new Error('Missing start script for ' + env));
+		}
 	});
 };
